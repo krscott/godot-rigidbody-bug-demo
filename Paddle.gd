f23d5c0211ab5d18fc442a45f36681ecad5d212a
Kris Scott <kscott91@gmail.com>
2020-08-20 07:37:07 -0500

    Add workaround fix

--- Paddle.gd
+++ Paddle.gd
@@ -7,7 +7,7 @@ export var workaround := true
 export var __info_label_path: NodePath
 onready var info_label := get_node(__info_label_path) as Label
 
-var force := 0.0
+var torque := 0.0
 
 
 func _ready() -> void:
@@ -15,23 +15,23 @@ func _ready() -> void:
 
 	if delay_force:
 		yield(get_tree().create_timer(1), "timeout")
-	force = -200.0
+	torque = -200.0
 
 
 func _process(_delta: float) -> void:
-	info_label.text = "apply_torque_impulse(%f)" % force
+	info_label.text = "apply_torque_impulse(%f)" % torque
 
 
 func _physics_process(_delta: float) -> void:
 	if workaround:
-		apply_torque_impulse_fixed(force)
+		apply_torque_impulse(torque)
 	else:
-		apply_torque_impulse(force)
+		.apply_torque_impulse(torque)
 
 
-func apply_torque_impulse_fixed(force: float) -> void:
-	apply_torque_impulse(force)
-	if not is_zero_approx(force):
+func apply_torque_impulse(torque: float) -> void:
+	.apply_torque_impulse(torque)
+	if not is_zero_approx(torque):
 		var original_can_sleep := can_sleep
 		can_sleep = false
 		can_sleep = original_can_sleep
