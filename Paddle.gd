c977f3a8ffaaac86dbe4a6b7dd809a10f2ffb77c
Kris Scott <kscott91@gmail.com>
2020-08-20 07:09:39 -0500

    Add workaround fix

--- Paddle.gd
+++ Paddle.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 
 export var delay_force := true
+export var workaround := true
 
 export var __info_label_path: NodePath
 onready var info_label := get_node(__info_label_path) as Label
@@ -22,4 +23,15 @@ func _process(_delta: float) -> void:
 
 
 func _physics_process(_delta: float) -> void:
+	if workaround:
+		apply_torque_impulse_fixed(force)
+	else:
+		apply_torque_impulse(force)
+
+
+func apply_torque_impulse_fixed(force: float) -> void:
 	apply_torque_impulse(force)
+	if not is_zero_approx(force):
+		var original_can_sleep := can_sleep
+		can_sleep = false
+		can_sleep = original_can_sleep
